b65aa82bda505cc4e3034ff41a71b233af84cfcf
Fhelipe Turati <37228377+fmturati@users.noreply.github.com>
2018-09-05 16:42:19 -0700

    Second Part of the TODO LIST

--- js/todo-project.js
+++ js/todo-project.js
@@ -1,19 +1,41 @@
 var todos = [];
+var todoIndex = todos.length;
 var input = prompt("What would you like to do?");
 
 while(input !== "quit"){
 
     if (input === "list"){
-        console.log(todos); 
-    } 
+        listTodo()
+    }     
     else if(input === "new"){
-        var newTodo = prompt("Enter new todo");
-        todos.push(newTodo);
-        
+        addTodo();    
     } 
+    else if (input === "delete"){
+        deleteTodo();
+    }
     var input = prompt("What would you like to do?");
     }
 
 console.log("OK, YOU QUIT THE APP");
+
+
+function listTodo(){
+    todos.forEach((item, i)=>{
+    console.log(i + ": " + item);
+    });
+    console.log("----------------------------");
+}
+
+function addTodo(){
+    var newTodo = prompt("Enter new todo");
+    todos.push(newTodo);   
+    console.log("Added Todo"); 
+}
+
+function deleteTodo(){
+    var deleteTodo = prompt("Enter the index number that you want to delete.")
+    todos.splice(deleteTodo,1);
+    console.log("Deleted Todo!")
+}
  
 window.setTimeout(function() {}, 10);
