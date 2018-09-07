568f79059aa355b06273a466354f5313995f13d9
Fhelipe Turati <37228377+fmturati@users.noreply.github.com>
2018-09-07 09:12:32 -0700

    Final part of the SCORE KEEPER game

--- js/scorekeeper.js
+++ js/scorekeeper.js
@@ -1,24 +1,57 @@
+var p1Btn = document.querySelector("#p1");
+var p2Btn = document.querySelector("#p2");
+var scoreP1Area = document.querySelector("#scorep1");
+var scoreP2Area = document.querySelector("#scorep2");
+var scoreP1 = 0;
+var scoreP2 = 0;
+var gameOver = false;
+var winningScore = 5;
 
-var p1 = document.querySelector("#p1");
-var p2 = document.querySelector("#p2");
-var scorep1Area = document.querySelector("#scorep1");
-var scorep2Area = document.querySelector("#scorep2");
-var scorep1 = 1;
-var scorep2 = 1;
-var reset = document.querySelector("#reset");
-
+var resetBtn = document.querySelector("#reset");
 var score = document.querySelector("#score");
-
-var input = 10;
 var inputArea = document.querySelector("#input");
-
 var goal = document.querySelector("#goal");
+var playTo = document.querySelector("#playTo");
 
-p2.addEventListener("click", function(){    
-    if (scorep1 <= input-1){
-        scorep2Area.textContent = scorep1++;
-    } else {
-        goal.textContent = "Player 2 WIN!"
-        scorep2Area.style.color = "green";
+
+p2Btn.addEventListener("click", function(){    
+    if (!gameOver){
+        scoreP2++;
+        if(scoreP2 === winningScore){                     
+            scoreP2Area.classList.add("winner");
+            gameOver = true;   
+        }  
+        scoreP2Area.textContent = scoreP2;
     }
+});
+
+p1Btn.addEventListener("click", function(){
+    if(!gameOver){
+        scoreP1++
+        if(scoreP1 === winningScore){                      
+            scoreP1Area.classList.add("winner");
+            gameOver = true; 
+        } 
+        scoreP1Area.textContent = scoreP1;
+    }
+});
+
+function reset(){
+    scoreP1 = 0;
+    scoreP2 = 0;
+    scoreP1Area.textContent = 0;
+    scoreP2Area.textContent = 0;
+    scoreP1Area.classList.remove("winner");
+    scoreP2Area.classList.remove("winner");
+    gameOver = false;     
+}
+
+resetBtn.addEventListener("click", function(){
+     reset();
+});
+
+inputArea.addEventListener("change", function(){
+    playTo.textContent = this.value;
+    winningScore = Number(this.value);
+    reset();
 });
